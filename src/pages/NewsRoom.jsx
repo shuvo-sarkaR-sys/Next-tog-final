b71933eb7f8f2024abcd541145db3e46ae98524e
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2025-05-18 18:26:33 +0600

    updated

--- src/pages/NewsRoom.jsx
+++ src/pages/NewsRoom.jsx
@@ -27,7 +27,7 @@ import FadeUp from '../components/FadeUp.jsx'
        
         {blogs.map((blog) => (
             <FadeUp key={blog._id}>
-             <div  className="relative group bg-white shadow-md rounded-2xl   overflow-hidden justify-center w-[320px]  h-[450px]">
+             <Link to={`/blog/${blog._id}`}> <div  className="relative group bg-white shadow-md rounded-2xl   overflow-hidden justify-center w-[320px]  h-[450px]">
                
               <img className='w-full object-cover transition-transform duration-700 ease-out group-hover:scale-110' src={blog.imageUrl} alt="" />
               
@@ -35,7 +35,7 @@ import FadeUp from '../components/FadeUp.jsx'
                  <p><strong>Publish Date: </strong>{new Date(blog.date).toLocaleDateString()}</p>
           <h1 className='text-xl h-8 overflow-hidden font-semibold' >{blog.title}</h1>
           <p className='text-gray-300 h-14 overflow-hidden group-hover:underline-offset-1'>{blog.description}</p></div>
-            </div></FadeUp> 
+            </div></Link></FadeUp> 
           
         ))}
     
